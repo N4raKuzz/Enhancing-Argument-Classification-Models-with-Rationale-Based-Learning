4c6f7c19143d4c198b8c61cd35153d7157b6a8f3
N4raKuzz <zzynrk@gmail.com>
2023-11-05 07:46:49 +0800

    Add Multihead Attention; Add FeedForward; Add Normalization

--- BasicTransformerModel/model.py
+++ BasicTransformerModel/model.py
@@ -47,41 +47,64 @@ class PositionalEncoding(nn.Module):
         x = x + (self.encoding[:, :seq_len, :])
         return self.dropout(x)
 
-
+class AddNorm(nn.Module):
+    def __init__(self, size, eps=1e-6):
+        super(AddNorm, self).__init__()
+        self.eps = eps
+        self.norm = nn.LayerNorm(size, eps=eps)
     
+    def forward(self, x, sublayer):       
+        return self.norm(x)
 
-class TransformerModel(nn.Module):
+class FeedForward(nn.Module):
 
-    def __init__(self, ntoken: int, d_model: int, nhead: int, d_hid: int,
-                 nlayers: int, dropout: float = 0.5):
+    def __init__(self, d_model: int, d_ff: int, dropout) -> None:
+        super().__init__()    
+        self.dropout = nn.Dropout(dropout)
+        
+        self.linear_1 = nn.Linear(d_model,d_ff) #W1 + Bias1
+        self.linear_2 = nn.Linear(d_ff,d_model) #W2 + Bias2
+
+    def forward(self, x):
+        #FNN(x) = ReLU(0, xW1 + b1)W2 + b2
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+    
+class MultiHeadAttetion(nn.Module):
+
+    def __init__(self, d_model: int, head: int, dropout: float) -> None:
         super().__init__()
-        self.model_type = 'Transformer'
-        self.pos_encoder = PositionalEncoding(d_model, dropout)
-        encoder_layers = TransformerEncoderLayer(d_model, nhead, d_hid, dropout)
-        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
-        self.embedding = nn.Embedding(ntoken, d_model)
         self.d_model = d_model
-        self.linear = nn.Linear(d_model, ntoken)
+        self.head = head
+        assert d_model % head == 0 # d_model has to be divisable by head
 
-        self.init_weights()
+        self.d_k = d_model // head
+        self.w_q = nn.Linear(d_model,d_model) # Wq Weight for Query
+        self.w_k = nn.Linear(d_model,d_model) # Wk Weight for Key
+        self.w_v = nn.Linear(d_model,d_model) # Wv Weight for Value
+        self.w_o = nn.Linear(d_model,d_model) # Wo Weight for Output
 
-    def init_weights(self) -> None:
-        initrange = 0.1
-        self.embedding.weight.data.uniform_(-initrange, initrange)
-        self.linear.bias.data.zero_()
-        self.linear.weight.data.uniform_(-initrange, initrange)
+        self.dropout = nn.Dropout(dropout)
 
-    def forward(self, src: Tensor, src_mask: Tensor = None) -> Tensor:
-        """
-        Arguments:
-            src: Tensor, shape ``[seq_len, batch_size]``
-            src_mask: Tensor, shape ``[seq_len, seq_len]``
+    def forward(self,q,k,v,mask):
 
-        Returns:
-            output Tensor of shape ``[seq_len, batch_size, ntoken]``
-        """
-        src = self.embedding(src) * math.sqrt(self.d_model)
-        src = self.pos_encoder(src)
-        output = self.transformer_encoder(src, src_mask)
-        output = self.linear(output)
-        return output
+        batch_size = q.size(0)
+        # Apply the linear transformations and split into `head` heads
+        query = self.w_q(q).view(batch_size, -1, self.head, self.d_k).transpose(1, 2)
+        key = self.w_k(k).view(batch_size, -1, self.head, self.d_k).transpose(1, 2)
+        value = self.w_v(v).view(batch_size, -1, self.head, self.d_k).transpose(1, 2)
+
+        # Attention(Q,K,V) = softmax(QK^T/d_k^0.5) V
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(self.d_k)
+        
+        if mask is not None:                 # Apply mask to multi-heads
+            attention_scores = attention_scores.masked_fill(mask == 0, float('-inf'))
+        
+        attention_scores = attention_scores.softmax(dim=-1)
+        attention_scores = self.dropout(attention_scores)
+
+        # Multihead(Q,K,V) = Concat(head1...head_n) Wo        
+        multihead = (attention_scores @ value).transpose(1, 2).contiguous().view(batch_size, -1, self.d_model)
+        multihead = self.w_o(multihead)
+
+        return multihead
+
